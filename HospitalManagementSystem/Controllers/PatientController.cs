0ce075c967d07786c831280d1952a71889897e7f
Fahimzz <shahariarfahim@gmail.com>
2021-08-10 21:04:30 +0600

    reference added with edmx

---
+++ HospitalManagementSystem/Controllers/PatientController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HospitalManagementSystem.Controllers
+{
+    public class PatientController : ApiController
+    {
+        [Route("api/patient/all")]
+        public List<string> GetAll()
+        {
+            return null;
+        }
+    }
+}
